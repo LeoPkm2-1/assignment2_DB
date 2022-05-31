f1f908fddc42678d90adeed4f33e7f91c8ae54ed
LeoPkm2-1 <89697898+LeoPkm2-1@users.noreply.github.com>
2022-05-31 17:00:54 +0700

    trigger ok

--- back-end/model/assignment2.sql
+++ back-end/model/assignment2.sql
@@ -43,20 +43,21 @@ create table employee (
   emp_address varchar(250) default null,
   emp_phone varchar(250) not null,
   emp_email varchar(50) default null,
-  emp_birthday year(4) not null,
+  emp_birthday date not null,
   emp_salary_per_hour decimal(10,3) default null,
   emp_password varchar(250) not null,
   emp_role_id int(6) unsigned not null,
   emp_branch_id int(6) unsigned not null,
   emp_shift_id int(6) unsigned not null,
   primary key (emp_id)
+
 );
 
 -- table giftcode
 drop table if exists giftcode;
 create table giftcode (
   gift_code_id int(6) unsigned not null auto_increment,
-  gift_code varchar(20) not null UNIQUE,
+  gift_code varchar(20) not null unique,
   gift_startdate datetime not null,
   gift_expiredate datetime not null,
   gift_value decimal(10,3) not null,
@@ -138,7 +139,7 @@ create table workhours (
   work_month int(5) not null,
   work_hours decimal(10,3) not null default 0.000,
   primary key (work_emp_id,work_month),
-  CONSTRAINT workhours_month_check_constraint CHECK (work_month>=1 and work_month<=12)
+  constraint workhours_month_check_constraint check (work_month>=1 and work_month<=12)
 );
 
 -- employee foreign key-- 
@@ -218,67 +219,261 @@ alter table workhours
   foreign key (work_emp_id) references employee(emp_id);
 
 
+-- trigger 
+-- drop trigger if exists employee_age_check_trigger;
+-- delimiter //
+-- create trigger employee_age_check_trigger
+-- 	before insert 
+--     on employee for each row
+--     begin 
+-- 		declare age integer default 0;
+--         declare finished integer default 0;        
+--         declare errormessage varchar(255) default '';
+--         declare newname varchar(255) default '';
+-- 		declare dateofbirth date;
+--         
+--         -- declare cursor for employee age
+--         declare cursorage 
+-- 			cursor for 
+-- 				select new.emp_birthday,new.emp_fullname from employee limit 1;
+--                 
+--         -- declare not found handler
+-- 		declare continue handler 
+-- 			for not found set finished = 1;
+--             
+-- 		open cursorage;
+--         
+--         checkage: loop
+-- 			fetch cursorage into dateofbirth,newname ;
+--             
+--             if finished = 1 then 
+-- 				leave checkage;
+-- 			end if;
+--             
+--             select date_format(from_days(datediff(now(),dateofbirth)), '%y')+0 
+-- 			into age
+-- 			from employee;
+--             
+-- 			set errormessage = concat('the age of employee ',newname,
+-- 										  ' is ', age,'less than 18'        
+-- 			);
+-- 			if age <18 then
+-- 				signal sqlstate '45001'
+-- 					set message_text= errormessage;
+-- 			end if;
+--         
+--         end loop checkage;
+--         close cursorage;    
+--     
+--     end//
+-- delimiter ;
+
+ -- trigger 
+drop trigger if exists employee_age_check_insert_trigger;
+delimiter //
+create trigger employee_age_check_insert_trigger
+	before insert 
+    on employee for each row
+    begin 
+		declare age integer  ;
+        declare namenew varchar(50) default '';
+        declare errormessage varchar(300) default '';
+        
+		-- select date_format(from_days(datediff(now(),new.emp_birthday)), '%y')+0 as ages
+        
+        
+       
+
+    --     select date_format(from_days(datediff(now(),emp_birthday)), '%y')+0 as ages ,emp_fullname
+    --     into age,namenew
+		-- from employee
+		-- where emp_id=new.emp_id;
+
+      set age=date_format(from_days(datediff(now(),new.emp_birthday)), '%y')+0;
+      set namenew=new.emp_fullname;
+
+  --         select year(new.emp_birthday),new.emp_fullname
+  --         into age,namenew
+  -- 		from employee
+  -- ;
+			
+
+		set errormessage = concat('the age of employee ',namenew,
+									  ' is ', age,' less than 18'        
+		);
+        
+		if age  < 18 then
+			 signal sqlstate '45000' 
+            set message_text = errormessage;
+		end if;
+        
+       
+    
+    end//
+delimiter ;
+
+drop trigger if exists employee_age_check_update_trigger;
+delimiter //
+create trigger employee_age_check_update_trigger
+	before update 
+    on employee for each row
+    begin 
+		declare age integer  ;
+        declare namenew varchar(50) default '';
+        declare errormessage varchar(300) default '';
+        
+--         select date_format(from_days(datediff(now(),emp_birthday)), '%y')+0 as ages ,emp_fullname
+--         into age,namenew
+-- 		from employee
+-- 		where emp_id=new.emp_id;
+        
+        set age = date_format(from_days(datediff(now(),new.emp_birthday)), '%y')+0;
+		set namenew=new.emp_fullname;
+		set errormessage = concat('the age is updated of employee ',namenew,
+									  ' is ', age,' less than 18'        
+		);
+        
+		if age  < 18 then
+			 signal sqlstate '45000' 
+            set message_text = errormessage;
+		end if;       
+       
+    
+    end//
+delimiter ;
+
+
+drop trigger if exists giftcode_time_check_insert_trigger;
+delimiter //
+create trigger giftcode_time_check_insert_trigger
+	after insert
+    on giftcode for each row
+    begin
+     DECLARE giftname varchar(200);
+    DECLARE startday datetime;
+    DECLARE endday datetime;
+    declare errormessage varchar(300) default '';
+    
+    select gift_startdate,gift_expiredate,gift_code
+    into startday,endday,giftname
+    from giftcode
+    where gift_code_id = new.gift_code_id;
+	
+	set errormessage = concat('the day of gift code: ',giftname,' start day ',startday,
+									  ' not suitable for end day:  ', endday,' of gift code'        
+		);
+    
+    if endday<=startday then
+		signal sqlstate '45000' 
+            set message_text = errormessage;
+    end if;
+    
+    end//
+delimiter ;
+
+drop trigger if exists giftcode_time_check_update_trigger;
+delimiter //
+  create trigger giftcode_time_check_update_trigger
+  before update on giftcode for each row
+  begin
+    DECLARE giftname varchar(200);
+    DECLARE startday datetime;
+    DECLARE endday datetime;
+    declare errormessage varchar(300) default '';
+
+
+    set giftname=new.gift_code;
+    set startday = new.gift_startdate;
+    set endday = new.gift_expiredate;
+
+	  set errormessage = concat('the day of gift update code: ',giftname,' start day ',startday,
+									  ' not suitable for end day:  ', endday,' of gift code'        
+		);
+
+    if endday<=startday then
+		signal sqlstate '45000' 
+            set message_text = errormessage;
+    end if;
+
+  end //
+delimiter ;
 
 -- insert data
-INSERT INTO branch (branch_name, brach_location)
-VALUES ('NYC', '111 Centre St, New York, NY 10013, United States'),
-('London','Exhibition Rd, South Kensington, London SW7 2BX, United Kingdom'),
-('Paris','24 Rue du Commandant Guilbaud, 75016 Paris, France'),
-('Madrid','de Concha Espina, 1, 28036 Madrid, Spain');
+insert into branch (branch_name, brach_location)
+values ('nyc', '111 centre st, new york, ny 10013, united states'),
+('london','exhibition rd, south kensington, london sw7 2bx, united kingdom'),
+('paris','24 rue du commandant guilbaud, 75016 paris, france'),
+('madrid','de concha espina, 1, 28036 madrid, spain');
 
 
-INSERT INTO category (category_name)
-VALUES ('Cake'),
-('Tea'),
-('Milk'),
-('Cafe'),
-('Ice cream');
+insert into category (category_name)
+values ('cake'),
+('tea'),
+('milk'),
+('cafe'),
+('ice cream');
 
-INSERT INTO `role` (role_name)
-VALUES ('barista'),
-('Waiter'),
+insert into `role` (role_name)
+values ('barista'),
+('waiter'),
 ('cashier');
 
-INSERT INTO customer (cus_fullname,cus_address,cus_phone,cus_email,cus_password)
-VALUES ('Will Smith','111 Centre St, New York','0147852369','WillSmith@gmail.com','null'),
-('Tom Hanks','111 Centre St, New York','0124552369','TomHanksh@gmail.com','null'),
-('Brad Pitt','111 Centre St, New York','0965852369','BradPitt@gmail.com','null');
+insert into customer (cus_fullname,cus_address,cus_phone,cus_email,cus_password)
+values ('will smith','111 centre st, new york','0147852369','willsmith@gmail.com','null'),
+('tom hanks','111 centre st, new york','0124552369','tomhanksh@gmail.com','null'),
+('brad pitt','111 centre st, new york','0965852369','bradpitt@gmail.com','null');
 
-INSERT INTO shift (shift_time)
-VALUES ('06:00-14:00'),
+insert into shift (shift_time)
+values ('06:00-14:00'),
 ('14:00-22:00');
 
-INSERT INTO employee (emp_fullname, emp_address, emp_phone, emp_email, emp_birthday, emp_salary_per_hour, emp_password, emp_role_id, emp_branch_id, emp_shift_id)
-VALUES ('Will Smith','Sir Matt Busby Way, Old Trafford, Stretford, Manchester','0147851234','WillSmith@gmail.com',1994,13.77,'null',1,2,1),
- ('Tom Hanks','111 Centre St, New York','0147851234','TomHanksh@gmail.com',1990,14.77,'null',2,1,1),
- ('Brad Pitt','Sir Matt Busby Way, Old Trafford, Stretford, Manchester','0965851234','BradPitt@gmail.com',1998,12.77,'null',1,2,2),
- ('Nguyen van teo', 'London SW1A 1AA, United Kingdom', '01478523547', 'ahihi2@gmail.com', 1995, 12.5, 'emp_password', 3, 1, 2),
- ('Hoang van ty', 'London EC3N 4AB, United Kingdom', '03698521456', 'ahihi3@gmail.com', 1995, 25.4, 'emp_password', 3, 1, 2),
- ('Luong trieu Vy', ' London, United Kingdom', '0123654852', 'ahihi4@gmail.com', 1995, 15.8, 'emp_password', 1, 2, 1),
- ('Luu duc hoa', 'Bd de Parc, 77700 Coupvray, France', '0147532159', 'ahihi6@gmail.com', 1995, 14.9, 'emp_password', 1, 2, 1);
+insert into employee (emp_fullname, emp_address, emp_phone, emp_email, emp_birthday, emp_salary_per_hour, emp_password, emp_role_id, emp_branch_id, emp_shift_id)
+values 
+ ('will smith','sir matt busby way, old trafford, stretford, manchester','0147851234','willsmith@gmail.com','1994-02-15',13.77,'null',1,2,1),
+ ('tom hanks','111 centre st, new york','0147851234','tomhanksh@gmail.com','1990-02-15',14.77,'null',2,1,1),
+ ('brad pitt','sir matt busby way, old trafford, stretford, manchester','0965851234','bradpitt@gmail.com','1998-02-15',12.77,'null',1,2,2),
+ ('nguyen van teo', 'london sw1a 1aa, united kingdom', '01478523547', 'ahihi2@gmail.com', '1999-01-15', 12.5, 'emp_password', 3, 1, 2),
+ ('hoang van ty', 'london ec3n 4ab, united kingdom', '03698521456', 'ahihi3@gmail.com', '1995-02-15', 25.4, 'emp_password', 3, 1, 2),
+ ('luong trieu vy', ' london, united kingdom', '0123654852', 'ahihi4@gmail.com', '1995-08-25', 15.8, 'emp_password', 1, 2, 1), 
+ ('luu duc hoa', 'bd de parc, 77700 coupvray, france', '0147532159', 'ahihi6@gmail.com', '1995-03-15', 14.9, 'emp_password', 1, 2, 1)
+ ;
 
-INSERT INTO product (product_name, product_listed_price, product_number, product_start_avg, product_category_id,product_image)
-VALUES ('Bacon, Sausage & Egg Wrap',12.5,150,2,1,'https://www.fastfoodpost.com/wp-content/uploads/2020/03/Starbucks-New-Southwest-Veggie-Wrap-And-New-Bacon-Sausage-Egg.jpg'),
-('Bacon, Gouda & Egg Sandwich',13.8,200,4,1,'https://i.dayj.com/image/720/food/1898448/starbucks-bacon-gouda-and-egg-sandwich-1-piece.png'),
-('Cold Brew Coffee', 8.2, 1000, 3, 4,'https://www.thespruceeats.com/thmb/gcBhhLtXnEd8JdM4zQDRSenkshI=/1885x1414/smart/filters:no_upscale()/coldbrewcoffeemicrogen-95e6ef2fc2c1411bbcf1cefe5e9e6879.jpg'),
-('Chocolate Cream Cold Brew', 10.2, 5000, 4.4, 4,'https://www.simplejoy.com/wp-content/uploads/2022/03/how-to-make-irish-cream-cold-brew-3-683x1024.jpg'),
-('Matcha Tea Latte', 9.8, 3000, 4, 2,'https://ostomyconnection.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTU1MzE4ODYyMDYwMDA0NTQw/green-macha-tea.jpg'),
-('Mango Dragonfruit Lemonade', 7.8, 500, 3.1, 2,'https://cookathomemom.com/wp-content/uploads/2021/04/Mango-Dragonfruit-Lemonade.jpg');
+
+insert into product (product_name, product_listed_price, product_number, product_start_avg, product_category_id,product_image)
+values ('bacon, sausage & egg wrap',12.5,150,2,1,'https://www.fastfoodpost.com/wp-content/uploads/2020/03/starbucks-new-southwest-veggie-wrap-and-new-bacon-sausage-egg.jpg'),
+('bacon, gouda & egg sandwich',13.8,200,4,1,'https://i.dayj.com/image/720/food/1898448/starbucks-bacon-gouda-and-egg-sandwich-1-piece.png'),
+('cold brew coffee', 8.2, 1000, 3, 4,'https://www.thespruceeats.com/thmb/gcbhhltxned8jdm4zqdrsenkshi=/1885x1414/smart/filters:no_upscale()/coldbrewcoffeemicrogen-95e6ef2fc2c1411bbcf1cefe5e9e6879.jpg'),
+('chocolate cream cold brew', 10.2, 5000, 4.4, 4,'https://www.simplejoy.com/wp-content/uploads/2022/03/how-to-make-irish-cream-cold-brew-3-683x1024.jpg'),
+('matcha tea latte', 9.8, 3000, 4, 2,'https://ostomyconnection.com/.image/ar_16:9%2cc_fill%2ccs_srgb%2cfl_progressive%2cg_faces:center%2cq_auto:good%2cw_768/mtu1mze4odyymdywmda0ntqw/green-macha-tea.jpg'),
+('mango dragonfruit lemonade', 7.8, 500, 3.1, 2,'https://cookathomemom.com/wp-content/uploads/2021/04/mango-dragonfruit-lemonade.jpg');
 
 insert into giftcode(gift_code,gift_startdate,gift_expiredate,gift_value)
-VALUES ('ahihi2022','2022-04-05 00:00:00','2022-08-05 00:00:00',12.7),
+values ('ahihi2022','2022-04-05 00:00:00','2022-08-05 00:00:00',12.7),
 ('ok2222','2022-05-05 00:00:00','2022-09-05 00:00:00',13.8),
-('heheh202','2022-05-25 00:00:00','2022-09-05 00:00:00',20.2);
+('heheh202','2022-05-25 00:00:00','2022-05-25 00:00:01',20.2);
 
 insert into managerbranch(emp_id, branch_id, startdate)
-VALUES (1, 4, '2020-09-01'),
+values (1, 4, '2020-09-01'),
 (2, 3, '2022-01-01'),
 (3, 2, '2021-03-01'),
 (4, 1, '2020-07-01');
 
 
 insert into workhours(work_emp_id, work_month, work_hours)
-VALUES (1, 4, 100.5),
+values (1, 4, 100.5),
 (2, 3,80.2 ),
 (3, 2,25.7 ),
 (4, 1,15.2 );
+
+-- UPDATE employee
+-- SET emp_birthday = '2021-01-01'
+-- WHERE emp_id=1;
+
+UPDATE giftcode
+SET gift_code ='test2'
+WHERE gift_code_id=1;
+
+-- insert into giftcode(gift_code,gift_startdate,gift_expiredate,gift_value)
+-- values ('ahihi2022','2022-04-05 00:00:00','2022-08-05 00:00:00',12.7),
+-- ('ok2222','2022-05-05 00:00:00','2022-09-05 00:00:00',13.8),
+-- ('heheh202','2022-05-25 00:00:00','2022-05-25 00:00:01',20.2);
