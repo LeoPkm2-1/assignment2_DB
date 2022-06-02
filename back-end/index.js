3c64b6bd5cf89d3a345ea6e9e9966fcf21b21e85
chituenguyen <tuenguyen553@gmail.com>
2022-06-02 09:24:02 +0700

    done api ab

--- back-end/index.js
+++ back-end/index.js
@@ -3,6 +3,7 @@ const express =require('express');
 const categoryRouter=require('./route/category');
 const productRouter=require('./route/product');
 const productorderRouter=require('./route/productorder');
+const orderRouter=require('./route/order')
 const app =express();
 app.use(express.json())
 const port =5000;
@@ -12,6 +13,7 @@ const port =5000;
 app.use('/api/category',categoryRouter);
 app.use('/api/product',productRouter);
 app.use('/api/productorder',productorderRouter);
+app.use('/api/order',orderRouter);
 
 
 app.listen(port,()=>{
