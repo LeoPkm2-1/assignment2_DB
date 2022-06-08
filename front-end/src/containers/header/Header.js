23d0c6eb46d5bb3fa08d46c0767ffb55135dfcb8
chituenguyen <tuenguyen553@gmail.com>
2022-06-08 17:51:15 +0700

    fix header

--- front-end/src/containers/header/Header.js
+++ front-end/src/containers/header/Header.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { StyledHeader } from './style';
+import './../../styles/header.css'
+import logo from'./../../image/thecoffeehouse.png'
 
 function Header() {
   return (
-    <StyledHeader>
-      <div className='logo'><h2>Coffee House</h2></div>
-      <div className='search'>
-        <form>
-          <input placeholder='Enter...'/>
-          <button type='button'>Search</button>
-        </form>
-      </div>
-      <div>
-        <Link to='../login'>Đăng nhập</Link>
-      </div>
-    </StyledHeader>
+    <div class="header">
+        <img src={logo} alt=""></img>
+        <ul class="list">
+            <li>Đặt hàng</li>
+            <li>Tin tức</li>
+            <li>Cừa hàng</li>
+            <li>Khuyến mãi</li>
+            <li>Tuyển dụng</li>
+        </ul>
+        <div class="icon">
+            <i class="fa-solid fa-user"></i>
+            <i class="fa-solid fa-cart-plus"></i>
+        </div>
+    </div>
   )
 }
 
